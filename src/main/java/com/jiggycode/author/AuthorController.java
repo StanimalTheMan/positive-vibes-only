60ac69e924efcf7e95090673871e5997dfdec9fb
StanimalTheMan <sjc591@nyu.edu>
2023-09-06 12:16:31 -0400

    Refactor controller api endpoint code, add post endpoint to add author

--- src/main/java/com/jiggycode/author/AuthorController.java
+++ src/main/java/com/jiggycode/author/AuthorController.java
@@ -1,12 +1,11 @@
 package com.jiggycode.author;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("api/v1/authors")
 public class AuthorController {
     private final AuthorService authorService;
 
@@ -19,14 +18,20 @@ public class AuthorController {
             path = "api/v1/author",
             method = RequestMethod.GET
     )*/
-    @GetMapping("api/v1/authors")
+    @GetMapping
     public List<Author> getAuthors() {
         return authorService.getAllAuthors();
     }
 
-    @GetMapping("api/v1/authors/{authorId}")
+    @GetMapping("{authorId}")
     public Author getAuthor(
             @PathVariable("authorId") Integer authorId) {
         return authorService.getAuthor(authorId);
+    }
+
+    @PostMapping
+    public void registerAuthor(
+            @RequestBody AuthorRegistrationRequest request) {
+        authorService.addAuthor(request);
     }
 }
