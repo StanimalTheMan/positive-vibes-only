bf81f2f68ee535f34a621fefcc904391398c260f
StanimalTheMan <sjc591@nyu.edu>
2023-09-23 15:02:46 -0400

    Use flyway dep to generate random authors upon app run

--- src/main/java/com/jiggycode/Main.java
+++ src/main/java/com/jiggycode/Main.java
@@ -1,5 +1,7 @@
 package com.jiggycode;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
 import com.jiggycode.author.Author;
 import com.jiggycode.author.AuthorRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
 public class Main {
@@ -19,20 +22,17 @@ public class Main {
 	@Bean
 	CommandLineRunner runner(AuthorRepository authorRepository) {
 		return args -> {
-			Author stanimal = new Author(
-					"Stanimal",
-					"stanimal@gmail.com",
-					26
+			var faker = new Faker();
+			Random random = new Random();
+			Name name = faker.name();
+			String firstName = name.firstName();
+			String lastName = name.lastName();
+			Author author = new Author(
+					firstName + " " + lastName,
+					firstName.toLowerCase() + "." + lastName.toLowerCase() + "@jiggycode.com",
+					random.nextInt(16, 99)
 			);
-
-			Author jiggy = new Author(
-					"Jiggy",
-					"jiggy@gmail.com",
-					25
-			);
-
-			List<Author> authors = List.of(stanimal, jiggy);
-			authorRepository.saveAll(authors);
+			authorRepository.save(author);
 		};
 	}
 
