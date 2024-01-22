044d209920727f5e8fcaf01f529fe364ca95b22d
StanimalTheMan <sjc591@nyu.edu>
2024-01-21 22:44:31 -0500

    Separate business logic from journal entry controller, add service class for it

--- src/main/java/com/jiggycode/journalentry/JournalEntryController.java
+++ src/main/java/com/jiggycode/journalentry/JournalEntryController.java
@@ -1,27 +1,22 @@
 package com.jiggycode.journalentry;
 
-import com.jiggycode.author.Author;
 import com.jiggycode.author.AuthorRepository;
-import com.jiggycode.exception.AuthorizationException;
 import com.jiggycode.exception.DuplicateResourceException;
-import com.jiggycode.exception.RequestValidationException;
 import com.jiggycode.exception.ResourceNotFoundException;
 import com.jiggycode.service.SentimentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import software.amazon.awssdk.services.comprehend.model.DetectSentimentResponse;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1")
 public class JournalEntryController {
+
+    @Autowired
+    private JournalEntryService journalEntryService;
 
     @Autowired
     private AuthorRepository authorRepository;
@@ -32,33 +27,16 @@ public class JournalEntryController {
     @Autowired
     private SentimentService sentimentService;
 
-    private boolean isAuthorizedAuthor(Integer authorId) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String currentUsername = authentication.getName();
-        return authorRepository.existsAuthorByIdAndEmail(authorId, currentUsername);
-    }
-
     @GetMapping("/authors/{authorId}/journal-entries")
     public ResponseEntity<List<JournalEntry>> getAllJournalEntriesByAuthorId(
             @PathVariable(value = "authorId") Integer authorId) {
-        if (!isAuthorizedAuthor(authorId)) {
-            throw new AuthorizationException("You are not authorized to view this resource.");
-        }
-
-        List<JournalEntry> journalEntries = journalEntryRepository.findByAuthorId(authorId);
+        List<JournalEntry> journalEntries = journalEntryService.getAllJournalEntriesByAuthorId(authorId);
         return new ResponseEntity<>(journalEntries, HttpStatus.OK);
     }
 
     @GetMapping("/journal-entries/{id}")
     public ResponseEntity<JournalEntry> getJournalEntryById(@PathVariable(value = "id") Integer id) {
-        JournalEntry journalEntry = journalEntryRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Did not find Journal Entry with id = " + id));
-
-        // Check if the authenticated user is the author of the requested entry
-        if (!isAuthorizedAuthor(journalEntry.getAuthor().getId())) {
-            throw new AuthorizationException("You are not authorized to view this resource.");
-        }
-
+        JournalEntry journalEntry = journalEntryService.getJournalEntryById(id);
         return new ResponseEntity<>(journalEntry, HttpStatus.OK);
     }
 
@@ -67,122 +45,42 @@ public class JournalEntryController {
             @PathVariable(value = "authorId") Integer authorId,
             @RequestBody JournalEntry journalEntryRequest)
     {
-        if (!isAuthorizedAuthor(authorId)) {
-            throw new AuthorizationException("You are not authorized to create a journal entry for this author.");
-        }
-
-        // Ensure that the provided authorId matches the author in the request
-        if (journalEntryRequest.getAuthor() != null && !Objects.equals(journalEntryRequest.getAuthor().getId(), authorId)) {
-            throw new RequestValidationException("Author ID in the path does not match the Author ID in the request body.");
-        }
-
-        // Set the author using the provided authorId
-        Author author = authorRepository.findById(authorId)
-                .orElseThrow(() -> new ResourceNotFoundException("Author not found with id = " + authorId));
-
-        // Set other fields in the journalEntryRequest
-        DetectSentimentResponse sentimentResponse;
-        try {
-            sentimentResponse = sentimentService.detectSentimentResponse(journalEntryRequest.getContent());
-        } catch (Exception e) {
-            throw new RuntimeException("Server error");
-        }
-
-        if (sentimentResponse.sentimentScore().positive() > 0.5f) {
-            journalEntryRequest.setAuthor(author);
-            journalEntryRequest.setCreationDate(journalEntryRequest.getCreationDate() != null ? journalEntryRequest.getCreationDate() : LocalDate.now());
-            journalEntryRequest.setUpdatedDate(journalEntryRequest.getCreationDate() != null ? journalEntryRequest.getCreationDate() : LocalDate.now());
-            journalEntryRequest.setPositiveSentimentScore(sentimentResponse.sentimentScore().positive());
-            journalEntryRequest.setNegativeSentimentScore(sentimentResponse.sentimentScore().negative());
-            journalEntryRequest.setNeutralSentimentScore(sentimentResponse.sentimentScore().neutral());
-            journalEntryRequest.setMixedSentimentScore(sentimentResponse.sentimentScore().mixed());
-
-            // Check for existing journal entry with the same creation date and author
-            if (journalEntryRepository.existsByCreationDateAndAuthor(journalEntryRequest.getCreationDate(), author)) {
-                throw new DuplicateResourceException("Journal entry with the same creation date already exists for this author.");
-            }
-        } else {
-            throw new RuntimeException("Invalid journal entry. Try to be more positive. Current positive sentiment score is less than 0.5: " + sentimentResponse.sentimentScore().positive());
-        }
-
-        // Save the journal entry
-        JournalEntry savedJournalEntry = journalEntryRepository.save(journalEntryRequest);
-
-        return new ResponseEntity<>(savedJournalEntry, HttpStatus.CREATED);
+        JournalEntry createdJournalEntry = journalEntryService.createJournalEntry(authorId, journalEntryRequest);
+        return new ResponseEntity<>(createdJournalEntry, HttpStatus.CREATED);
     }
 
     @PutMapping("/journal-entries/{id}")
     public ResponseEntity<JournalEntry> updateJournalEntry(@PathVariable("id") Integer id, @RequestBody JournalEntryUpdateRequest updateRequest) {
-        JournalEntry journalEntry = journalEntryRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("JournalEntryId " + id + " not found"));
-
-        // Check if the authenticated user is the author of the requested entry
-        if (!isAuthorizedAuthor(journalEntry.getAuthor().getId())) {
-            throw new AuthorizationException("You are not authorized to update this resource.");
-        }
-
-        boolean changes = false;
-
-        if (updateRequest.content() != null && !updateRequest.content().equals(journalEntry.getContent())) {
-            DetectSentimentResponse sentimentResponse;
-            try {
-                sentimentResponse = sentimentService.detectSentimentResponse(updateRequest.content());
-            } catch (Exception e) {
-                // Handle exceptions and error responses here
-                throw new RuntimeException("server error");
-            }
-            if (sentimentResponse.sentimentScore().positive() > 0.5f) {
-                journalEntry.setContent(updateRequest.content());
-                journalEntry.setSubject(updateRequest.subject());
-                journalEntry.setUpdatedDate(LocalDate.now());
-                journalEntry.setPositiveSentimentScore(sentimentResponse.sentimentScore().positive());
-                journalEntry.setNegativeSentimentScore(sentimentResponse.sentimentScore().negative());
-                journalEntry.setNeutralSentimentScore(sentimentResponse.sentimentScore().neutral());
-                journalEntry.setMixedSentimentScore(sentimentResponse.sentimentScore().mixed());
-            } else {
-                throw new RuntimeException("Invalid journal entry.  Try to be more positive.  Current positive sentiment score is less than 0.5: " + sentimentResponse.sentimentScore().positive());
-            }
-            changes = true;
-        }
-
-        if (updateRequest.subject() != null && !updateRequest.subject().equals(journalEntry.getSubject())) {
-            journalEntry.setSubject(updateRequest.subject());
-            changes = true;
-        }
-
-        if (!changes) {
-            throw new RequestValidationException("no data changes found");
-        }
-
-        return new ResponseEntity<>(journalEntryRepository.save(journalEntry), HttpStatus.OK);
+        JournalEntry updatedJournalEntry = journalEntryService.updateJournalEntry(id, updateRequest);
+        return new ResponseEntity<>(updatedJournalEntry, HttpStatus.OK);
     }
 
     @DeleteMapping("/journal-entries/{id}")
     public ResponseEntity<HttpStatus> deleteJournalEntry(@PathVariable("id") Integer id) {
-        JournalEntry journalEntry = journalEntryRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Did not find journal entry with id " + id));
-
-        // Check if the authenticated user is the author of the requested entry
-        if (!isAuthorizedAuthor(journalEntry.getAuthor().getId())) {
-            throw new AuthorizationException("You are not authorized to delete this resource.");
-        }
-
-        journalEntryRepository.deleteById(id);
-
+        journalEntryService.deleteJournalEntry(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/authors/{authorId}/journal-entries")
     public ResponseEntity<HttpStatus> deleteAllJournalEntriesOfAuthor(@PathVariable(value = "authorId") Integer authorId) {
-        if (!authorRepository.existsById(authorId)) {
-            throw new ResourceNotFoundException("Did not find author with id = " + authorId);
-        }
-
-        if (!isAuthorizedAuthor(authorId)) {
-            throw new AuthorizationException("You are not authorized to delete journal entries for this author.");
-        }
-
-        journalEntryRepository.deleteByAuthorId(authorId);
+        journalEntryService.deleteAllJournalEntriesOfAuthor(authorId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    // Exception handler for ResourceNotFoundException
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<String> handleDuplicateResourceException(DuplicateResourceException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    // Exception handler for other exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
---
+++ src/main/java/com/jiggycode/journalentry/JournalEntryService.java
@@ -0,0 +1,172 @@
+package com.jiggycode.journalentry;
+import com.jiggycode.author.Author;
+import com.jiggycode.author.AuthorRepository;
+import com.jiggycode.exception.AuthorizationException;
+import com.jiggycode.exception.DuplicateResourceException;
+import com.jiggycode.exception.RequestValidationException;
+import com.jiggycode.exception.ResourceNotFoundException;
+import com.jiggycode.service.SentimentService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.comprehend.model.DetectSentimentResponse;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class JournalEntryService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    @Autowired
+    private SentimentService sentimentService;
+
+    public List<JournalEntry> getAllJournalEntriesByAuthorId(Integer authorId) {
+        if (isAuthorizedAuthor(authorId)) {
+            throw new AuthorizationException("You are not authorized to view journal entries for this author.");
+        }
+
+        return journalEntryRepository.findByAuthorId(authorId);
+    }
+
+    public JournalEntry getJournalEntryById(Integer id) {
+        JournalEntry journalEntry = journalEntryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Did not find Journal Entry with id = " + id));
+
+        if (isAuthorizedAuthor(journalEntry.getAuthor().getId())) {
+            throw new AuthorizationException("You are not authorized to view this resource.");
+        }
+
+        return journalEntry;
+    }
+
+    public JournalEntry createJournalEntry(Integer authorId, JournalEntry journalEntryRequest) {
+        if (isAuthorizedAuthor(authorId)) {
+            throw new AuthorizationException("You are not authorized to create a journal entry for this author.");
+        }
+
+        validateAuthorId(authorId, journalEntryRequest);
+
+        Author author = authorRepository.findById(authorId)
+                .orElseThrow(() -> new ResourceNotFoundException("Author not found with id = " + authorId));
+
+        setJournalEntryFields(journalEntryRequest, author);
+
+        if (journalEntryRepository.existsByCreationDateAndAuthor(journalEntryRequest.getCreationDate(), author)) {
+            throw new DuplicateResourceException("Journal entry with the same creation date already exists for this author.");
+        }
+
+        return journalEntryRepository.save(journalEntryRequest);
+    }
+
+    private void validateAuthorId(Integer authorId, JournalEntry journalEntryRequest) {
+        if (journalEntryRequest.getAuthor() != null && !Objects.equals(journalEntryRequest.getAuthor().getId(), authorId)) {
+            throw new RequestValidationException("Author ID in the path does not match the Author ID in the request body.");
+        }
+    }
+
+    private void setJournalEntryFields(JournalEntry journalEntryRequest, Author author) {
+        DetectSentimentResponse sentimentResponse;
+        try {
+            sentimentResponse = sentimentService.detectSentimentResponse(journalEntryRequest.getContent());
+        } catch (Exception e) {
+            throw new RuntimeException("Server error");
+        }
+
+        if (sentimentResponse.sentimentScore().positive() > 0.5f) {
+            journalEntryRequest.setAuthor(author);
+            journalEntryRequest.setCreationDate(journalEntryRequest.getCreationDate() != null ? journalEntryRequest.getCreationDate() : LocalDate.now());
+            journalEntryRequest.setUpdatedDate(journalEntryRequest.getCreationDate() != null ? journalEntryRequest.getCreationDate() : LocalDate.now());
+            journalEntryRequest.setPositiveSentimentScore(sentimentResponse.sentimentScore().positive());
+            journalEntryRequest.setNegativeSentimentScore(sentimentResponse.sentimentScore().negative());
+            journalEntryRequest.setNeutralSentimentScore(sentimentResponse.sentimentScore().neutral());
+            journalEntryRequest.setMixedSentimentScore(sentimentResponse.sentimentScore().mixed());
+        } else {
+            throw new RuntimeException("Invalid journal entry. Try to be more positive. Current positive sentiment score is less than 0.5: " + sentimentResponse.sentimentScore().positive());
+        }
+    }
+
+    public JournalEntry updateJournalEntry(Integer id, JournalEntryUpdateRequest updateRequest) {
+        JournalEntry journalEntry = journalEntryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("JournalEntryId " + id + " not found"));
+
+        if (isAuthorizedAuthor(journalEntry.getAuthor().getId())) {
+            throw new AuthorizationException("You are not authorized to update this resource.");
+        }
+
+        boolean changes = false;
+
+        if (updateRequest.content() != null && !updateRequest.content().equals(journalEntry.getContent())) {
+            updateJournalEntryContent(journalEntry, updateRequest);
+            changes = true;
+        }
+
+        if (updateRequest.subject() != null && !updateRequest.subject().equals(journalEntry.getSubject())) {
+            journalEntry.setSubject(updateRequest.subject());
+            changes = true;
+        }
+
+        if (!changes) {
+            throw new RequestValidationException("No data changes found");
+        }
+
+        return journalEntryRepository.save(journalEntry);
+    }
+
+    private void updateJournalEntryContent(JournalEntry journalEntry, JournalEntryUpdateRequest updateRequest) {
+        DetectSentimentResponse sentimentResponse;
+        try {
+            sentimentResponse = sentimentService.detectSentimentResponse(updateRequest.content());
+        } catch (Exception e) {
+            throw new RuntimeException("Server error");
+        }
+
+        if (sentimentResponse.sentimentScore().positive() > 0.5f) {
+            journalEntry.setContent(updateRequest.content());
+            journalEntry.setSubject(updateRequest.subject());
+            journalEntry.setUpdatedDate(LocalDate.now());
+            journalEntry.setPositiveSentimentScore(sentimentResponse.sentimentScore().positive());
+            journalEntry.setNegativeSentimentScore(sentimentResponse.sentimentScore().negative());
+            journalEntry.setNeutralSentimentScore(sentimentResponse.sentimentScore().neutral());
+            journalEntry.setMixedSentimentScore(sentimentResponse.sentimentScore().mixed());
+        } else {
+            throw new RuntimeException("Invalid journal entry. Try to be more positive. Current positive sentiment score is less than 0.5: " + sentimentResponse.sentimentScore().positive());
+        }
+    }
+
+    public void deleteJournalEntry(Integer id) {
+        JournalEntry journalEntry = journalEntryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Did not find journal entry with id " + id));
+
+        if (isAuthorizedAuthor(journalEntry.getAuthor().getId())) {
+            throw new AuthorizationException("You are not authorized to delete this resource.");
+        }
+
+        journalEntryRepository.deleteById(id);
+    }
+
+    public void deleteAllJournalEntriesOfAuthor(Integer authorId) {
+        if (!authorRepository.existsById(authorId)) {
+            throw new ResourceNotFoundException("Did not find author with id = " + authorId);
+        }
+
+        if (isAuthorizedAuthor(authorId)) {
+            throw new AuthorizationException("You are not authorized to delete journal entries for this author.");
+        }
+
+        journalEntryRepository.deleteByAuthorId(authorId);
+    }
+
+    private boolean isAuthorizedAuthor(Integer authorId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        return !authorRepository.existsAuthorByIdAndEmail(authorId, currentUsername);
+    }
+}
